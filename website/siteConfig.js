7a11a7dfcd7b79adf6fc4c69951ef8999ac60bb0
Cody Fagley <cfagley@codeus.tech>
2019-12-04 19:40:18 -0700

    Attempt to fix CSS glitch

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -14,7 +14,7 @@ const siteConfig = {
   tagline: 'Securing the Internet of Things',
   disableTitleTagline: true,
   url: 'https://CodeusTech.github.io', // Your website URL
-  baseUrl: '/codeus-tech/', // Base URL for your project */
+  baseUrl: '/codeus-website/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
